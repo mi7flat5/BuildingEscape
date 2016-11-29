738097bab5e2a232553105f266e106ccecdb30d2
Shane Harmon <mi7flat5@gmail.com>
2016-11-29 20:25:41 +0100

    Added class to demonstrate code manipulation of object's rotation, in this case a door.

---
+++ BuildingEscape/Source/BuildingEscape/OpenDoor.cpp
@@ -0,0 +1,37 @@
+// Copyright Shane Harmon 2016
+
+#include "BuildingEscape.h"
+#include "OpenDoor.h"
+
+
+// Sets default values for this component's properties
+UOpenDoor::UOpenDoor()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UOpenDoor::BeginPlay()
+{
+	Super::BeginPlay();
+	AActor* owner = GetOwner();
+	FRotator rotation = owner->GetActorRotation();
+	FRotator newRotation = FRotator(0.0f,-60.0f,0.0f);
+	// ...
+	owner->SetActorRotation(newRotation);
+}
+
+
+// Called every frame
+void UOpenDoor::TickComponent( float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction )
+{
+	Super::TickComponent( DeltaTime, TickType, ThisTickFunction );
+
+	// ...
+}
+
