8af5d8cf35a6230e77c2b8d258c07a4ff4e5f5e1
Shane Harmon <mi7flat5@gmail.com>
2016-11-30 11:13:21 +0100

    Added macros for variables to be viewable and editable in the editor, and added a trigger volume.

--- BuildingEscape/Source/BuildingEscape/OpenDoor.h
+++ BuildingEscape/Source/BuildingEscape/OpenDoor.h
@@ -20,7 +20,13 @@ public:
 	
 	// Called every frame
 	virtual void TickComponent( float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction ) override;
+private: 
 
-		
+	UPROPERTY(VisibleAnywhere)
+	float openAngle = 90.0f;
+	
+	UPROPERTY(EditAnywhere)
+	ATriggerVolume* pressurePlate;
+
 	
 };
